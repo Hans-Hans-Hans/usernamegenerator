d687df53b8fa89e3917a40e905567284d85559e2
Hans <hans.automationlab@gmail.com>
2025-05-31 19:17:39 -0400

    Add user input and validation

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from typing import List
+import re
 
-def username_generator(name: str) -> List(str):
-    cleanedUsername = name.strip().split()
+def is_valid_name(name: str) -> bool:
+    usernamePattern = r"^[A-Za-z]+(?:[-'][A-Za-z]+)*\s+[A-Za-z]+(?:[-'][A-Za-z]+)*$"
+    return bool(re.match(usernamePattern, name))
+
+def username_generator(name: str) -> List[str]:
+    cleanedUsername = []
+
+    if is_valid_name(name):
+        cleanedUsername = name.strip().split()
+    else:
+        print("Please input a valid name...")
+        return username_generator(input("Please input a first and last name: "))
 
     firstname = cleanedUsername[0]
     lastname = cleanedUsername[1]
 
+
     print(f"Firstname: {firstname}, Lastname: {lastname}")
 
 if __name__ == "__main__":
-    username_generator("John Doe")
+    username_generator(input("Please input a first and last name: "))
