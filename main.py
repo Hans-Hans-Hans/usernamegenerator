dd31112b82f669e5e08723036f12d060fdbd6a0c
Hans <hans.automationlab@gmail.com>
2025-05-31 19:30:26 -0400

    Add username generation and filtering

--- main.py
+++ main.py
@@ -14,11 +14,44 @@ def username_generator(name: str) -> List[str]:
         print("Please input a valid name...")
         return username_generator(input("Please input a first and last name: "))
 
-    firstname = cleanedUsername[0]
-    lastname = cleanedUsername[1]
+    firstname = cleanedUsername[0].replace("'", "").replace("-", "")
+    lastname = cleanedUsername[1].replace("'", "").replace("-", "")
+
+    firstnameUpper = firstname.upper()
+    lastnameUpper = lastname.upper()
+
+    firstnameLower = firstname.lower()
+    lastnameLower = lastname.lower()
+
+    firstIUpper = firstname[0].upper()
+    lastIUpper = lastname[0].upper()
+
+    firstILower = firstname[0].lower()
+    lastILower = lastname[0].lower()
+
+    firstnames = [firstname, firstnameUpper, firstnameLower, firstIUpper, firstILower]
+    lastnames = [lastname, lastnameUpper, lastnameLower, lastIUpper, lastILower]
+
+    connectors = [".", "-", "_"]
+    finalUsernameList = []
+
+    for fname in firstnames:
+        for lname in lastnames:
+            finalUsernameList.append(fname + lname)
+            finalUsernameList.append(lname + fname)
+            for connector in connectors:
+                finalUsernameList.append(fname + connector + lname)
+                finalUsernameList.append(lname + connector + fname)
+
+    filteredUsernames = []
+
+    for usernames in finalUsernameList:
+        if len(usernames) > 3:
+            filteredUsernames.append(usernames)
 
 
-    print(f"Firstname: {firstname}, Lastname: {lastname}")
+
+    print(f"Here is your list of potential usernames: {filteredUsernames}")
 
 if __name__ == "__main__":
     username_generator(input("Please input a first and last name: "))
